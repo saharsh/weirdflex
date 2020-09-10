1a7801fdd92ebe1b1ffb89d55e782350306d3a69
Saharsh Yeruva <hello@saharsh.tech>
2020-09-10 13:21:58 -0400

    Trying something

--- api/weirdflex.js
+++ api/weirdflex.js
@@ -1,20 +1,20 @@
-const fetch = require('node-fetch');
+const fetch = require('fetch');
 
-export default (req, res) => {
-  const { token, text, user_id, response_url } = req.body;
-​
-  if (token !== process.env.SLACK_TOKEN) {
-    return res.status(400);
-  }
-	
-  res.status(200).send();
-	
-    await fetch(response_url, {
-	  method: 'POST',
-	  body: JSON.stringify({
-               response_type: 'in_channel',
-      text: 'hi'
-  })})
-​
-  return;
-}
+const getFlex = async (req, res) => {
+	const { token, response_url } = req.body;
+	if (token !== process.env.SLACK_TOKEN) {
+		return res.status(400);
+	}
+
+	res.status(200).send();
+
+	await fetch(response_url, {
+		method: 'POST',
+		body: JSON.stringify({
+			response_type: 'in_channel',
+			text: 'hi',
+		}),
+	});
+};
+
+getFlex();
