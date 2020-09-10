bfee0f3a1d9032c21ea62762f0e4ba01a79f13e3
Saharsh Yeruva <hello@saharsh.tech>
2020-09-10 01:51:59 -0400

    Fix env vars

--- api/weirdflex.js
+++ api/weirdflex.js
@@ -3,7 +3,7 @@ const fetch = require('node-fetch');
 export default (req, res) => {
   const { token, text, user_id, response_url } = req.body;
 ​
-  if (token !== process.env.SLACK_VERIFICATION_TOKEN) {
+  if (token !== process.env.SLACK_TOKEN) {
     return res.status(400);
   }
 	
