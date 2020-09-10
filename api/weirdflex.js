8ea9638933923595315376313fa1aaabd9d8c1b7
Saharsh Yeruva <hello@saharsh.tech>
2020-09-10 13:06:18 -0400

    just trying stuff out

--- api/weirdflex.js
+++ api/weirdflex.js
@@ -1,18 +1,18 @@
 const fetch = require('node-fetch');
 
 export default (req, res) => {
-  const { token, text, user_id, response_url } = req.body;
-​
+  const { token, text, user_id, response_url } = JSON.stringify(req.body);
+  
   if (token !== process.env.SLACK_TOKEN) {
     return res.status(400);
   }
 	
   res.status(200).send();
 	
-    await fetch(response_url, {
+  await fetch(response_url, {
 	  method: 'POST',
 	  body: JSON.stringify({
-               response_type: 'in_channel',
+      response_type: 'in_channel',
       text: 'hi'
   })})
 ​
